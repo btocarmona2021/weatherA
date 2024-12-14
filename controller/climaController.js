5ab26151dee101b72d66fda1c77c20c31f853900
Alberto C <btocarmona@gmail.com>
2024-12-14 02:23:40 -0300

    Ahora si App Terminada

--- controller/climaController.js
+++ controller/climaController.js
@@ -1,15 +1,15 @@
-import weather from "weather-js";
-
 export const obtenerClima = (req, res) => {
+    const { ciudad, pais } = req.query;
 
-    const ciudad = req.query.ciudad;
-    const provincia = req.query.pais;
+    if (!ciudad || !pais) {
+        return res.status(400).json({ error: 'Se requiere los parámetros "ciudad" y "pais".' });
+    }
 
-    weather.find({search: `${ciudad},${provincia}`, degreeType: 'C'}, (error, resultado) => {
+    weather.find({ search: `${ciudad},${pais}`, degreeType: 'C' }, (error, resultado) => {
         if (error) {
             console.log(error);
+            return res.status(500).json({ error: 'Hubo un problema al obtener el clima' });
         }
-        res.status(200).send(resultado[0]);
-
-    })
-}
+        res.status(200).json(resultado[0]);
+    });
+};
